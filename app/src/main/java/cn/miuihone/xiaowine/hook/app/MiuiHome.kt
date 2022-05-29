0ce008e107a79d0f9b9c11e27190e88bb901846a
xiaowine <xiaowine0gmail.com>
2022-05-29 16:07:07 +0800

    init

--- app/src/main/java/cn/miuihone/xiaowine/hook/app/MiuiHome.kt
+++ app/src/main/java/cn/miuihone/xiaowine/hook/app/MiuiHome.kt
@@ -2,10 +2,10 @@ package cn.miuihone.xiaowine.hook.app
 
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Environment
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import cn.miuihone.xiaowine.hook.BaseHook
@@ -13,51 +13,56 @@ import cn.miuihone.xiaowine.utils.LogUtils
 import cn.miuihone.xiaowine.utils.MemoryUtils
 import cn.miuihone.xiaowine.utils.Utils.catchNoClass
 import cn.miuihone.xiaowine.utils.Utils.formatSize
-import cn.miuihone.xiaowine.utils.Utils.isNull
 import com.github.kyuubiran.ezxhelper.init.InitFields.appContext
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.getObjectAs
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
-import com.github.kyuubiran.ezxhelper.utils.putObject
 
 
 @SuppressLint("StaticFieldLeak")
 object MiuiHome : BaseHook() {
-    private var memoryView: TextView? = null
+    private lateinit var mTxtMemoryViewGroup: ViewGroup
+    private lateinit var mTxtMemoryInfo1: TextView
+    private lateinit var memoryView: TextView
     @SuppressLint("SetTextI18n") override fun init() {
         catchNoClass {
             findMethod("com.miui.home.recents.views.RecentsContainer") { name == "refreshMemoryInfo" }.hookAfter {
-                val mTxtMemoryInfo1 = it.thisObject.getObjectAs<TextView>("mTxtMemoryInfo1")
-                val memoryInfoLayout = (mTxtMemoryInfo1.parent as LinearLayout)
-                for (i in 0 until memoryInfoLayout.childCount) {
-                    LogUtils.i(i.isNull())
-                    LogUtils.i(memoryInfoLayout.getChildAt(i))
-                    memoryInfoLayout.getChildAt(i).visibility = View.GONE
-                }
-                it.thisObject.putObject("mTxtMemoryInfo1", TextView(appContext))
-
-
-                val memoryInfo = MemoryUtils().getMemoryInfo(appContext)
-                val storageInfo = MemoryUtils().getStorageInfo(Environment.getExternalStorageDirectory())
-                val swapInfo: MemoryUtils = MemoryUtils().getPartitionInfo("SwapTotal", "SwapFree")
-
-                val memoryLayout = LinearLayout(appContext).apply {
-                    gravity = Gravity.START
-                    orientation = LinearLayout.VERTICAL
-                }
-                memoryView.isNull {
-                    memoryView = TextView(appContext).apply {
-                        setTextColor(Color.parseColor("#FFFFFF"))
+                LogUtils.i(isInit)
+                if (isInit) {
+                    mTxtMemoryViewGroup = it.thisObject.getObjectAs("mTxtMemoryContainer")
+                    LogUtils.i(mTxtMemoryViewGroup.childCount)
+                    for (i in 0 until mTxtMemoryViewGroup.childCount) {
+                        mTxtMemoryViewGroup.getChildAt(i).visibility = View.GONE
                     }
+                    mTxtMemoryInfo1 = it.thisObject.getObjectAs("mTxtMemoryInfo1")
+                    initView()
                 }
-                val a = memoryInfo.availMem.formatSize()
-                LogUtils.i(a)
-                memoryView!!.text = "可用：${a} | 总共：${memoryInfo.totalMem.formatSize()}"
-                memoryLayout.addView(memoryView)
-                (mTxtMemoryInfo1.parent as LinearLayout).addView(memoryLayout)
-
-
+                LogUtils.i(memoryView.text)
+                refreshDate()
+                LogUtils.i(memoryView.text)
             }
         }
+    }
+
+    private fun initView() {
+        val memoryLayout = LinearLayout(appContext).apply {
+            gravity = Gravity.START
+            orientation = LinearLayout.VERTICAL
+        }
+        memoryView = TextView(appContext).apply {
+            setTextColor(mTxtMemoryInfo1.textColors)
+            textSize = mTxtMemoryInfo1.textSize
+            typeface = mTxtMemoryInfo1.typeface
+        }
+        LogUtils.i(memoryView.text)
+        memoryLayout.addView(memoryView)
+        mTxtMemoryViewGroup.addView(memoryLayout)
+    }
+
+    @SuppressLint("SetTextI18n") private fun refreshDate() {
+        val memoryInfo = MemoryUtils().getMemoryInfo(appContext)
+        val storageInfo = MemoryUtils().getStorageInfo(Environment.getExternalStorageDirectory())
+        val swapInfo: MemoryUtils = MemoryUtils().getPartitionInfo("SwapTotal", "SwapFree")
+        memoryView.text = "可用：${memoryInfo.availMem.formatSize()} | 总共：${memoryInfo.totalMem.formatSize()}\n" + "可用：${storageInfo.availMem.formatSize()} | 总共：${storageInfo.totalMem.formatSize()}\n" + "可用：${swapInfo.availMem.formatSize()} | 总共：${swapInfo.totalMem.formatSize()}\n"
     }
 }
--- app/src/main/java/cn/miuihone/xiaowine/utils/Utils.kt
+++ app/src/main/java/cn/miuihone/xiaowine/utils/Utils.kt
@@ -1,21 +1,26 @@
 package cn.miuihone.xiaowine.utils
 
 import android.text.format.Formatter
+import cn.miuihone.xiaowine.hook.app.MiuiHome
 import com.github.kyuubiran.ezxhelper.init.InitFields
 
 
 object Utils {
-     fun catchNoClass(callback: () -> Unit) {
+    fun catchNoClass(callback: () -> Unit) {
         try {
             callback()
         } catch (e: NoSuchMethodException) {
             LogUtils.i("${e.message} 未找到class：${callback.javaClass.name}")
         }
     }
-    fun Any?.formatSize(): String =  Formatter.formatFileSize(InitFields.appContext, this as Long)
+
+    fun Any?.formatSize(): String = Formatter.formatFileSize(InitFields.appContext, this as Long)
+
+
     fun Any?.isNull(callback: () -> Unit) {
         if (this == null) callback()
     }
+
     fun Any?.isNotNull(callback: () -> Unit) {
         if (this != null) callback()
     }
