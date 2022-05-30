42f32d6bb1ad0592288389505c808549d7a53636
xiaowine <xiaowine0gmail.com>
2022-05-30 21:59:06 +0800

    破烂玩意

--- app/src/main/java/cn/miuihone/xiaowine/hook/app/MiuiHome.kt
+++ app/src/main/java/cn/miuihone/xiaowine/hook/app/MiuiHome.kt
@@ -17,26 +17,27 @@ import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.getObjectAs
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.github.kyuubiran.ezxhelper.utils.putObject
+import de.robv.android.xposed.XC_MethodHook
+import java.util.HashMap
 
 
 @SuppressLint("StaticFieldLeak")
 object MiuiHome : BaseHook() {
+    private var TextViewMaps = HashMap<String, TextView>().apply {
+        put("MemoryView", newTextView())
+        put("StorageView", newTextView())
+        put("ZarmView", newTextView())
+    }
     private var mInit: Boolean = false
     private lateinit var mTxtMemoryViewGroup: ViewGroup
     private lateinit var mTxtMemoryInfo1: TextView
-    private lateinit var MemoryView: TextView
-    private lateinit var StorageView: TextView
-    private lateinit var ZarmView: TextView
 
     @SuppressLint("SetTextI18n") override fun init() {
         catchNoClass {
             findMethod("com.miui.home.recents.views.RecentsContainer") { name == "refreshMemoryInfo" }.hookAfter {
                 if (!mInit) {
                     mTxtMemoryViewGroup = it.thisObject.getObjectAs("mTxtMemoryContainer")
-                    it.thisObject.putObject("mSeparatorForMemoryInfo", View(appContext))
-                    for (i in 0 until mTxtMemoryViewGroup.childCount) {
-                        mTxtMemoryViewGroup.getChildAt(i).visibility = View.GONE
-                    }
+                    hideView(it)
                     mTxtMemoryInfo1 = it.thisObject.getObjectAs("mTxtMemoryInfo1")
                     initView()
                     mInit = true
@@ -46,31 +47,36 @@ object MiuiHome : BaseHook() {
         }
     }
 
+    @SuppressLint("SetTextI18n") private fun refreshDate() {
+        val memoryInfo = MemoryUtils().getMemoryInfo(appContext)
+        val storageInfo = MemoryUtils().getStorageInfo(Environment.getExternalStorageDirectory())
+        val swapInfo: MemoryUtils = MemoryUtils().getPartitionInfo("SwapTotal", "SwapFree")
+        TextViewMaps["MemoryView"]!!.text = "运存可用：\t${memoryInfo.availMem.formatSize()} \t| \t总共：\t${memoryInfo.totalMem.formatSize()}"
+        TextViewMaps["ZarmView"]!!.text = "虚拟可用：\t${swapInfo.availMem.formatSize()} \t|\t总共：${swapInfo.totalMem.formatSize()}"
+        TextViewMaps["StorageView"]!!.text = "存储可用：\t${storageInfo.availMem.formatSize()} \t|\t总共：\t${storageInfo.totalMem.formatSize()}"
+    }
+
     private fun initView() {
         val memoryLayout = LinearLayout(appContext).apply {
             gravity = Gravity.CENTER
             orientation = LinearLayout.VERTICAL
         }
-        MemoryView = newTextView()
-        StorageView = newTextView()
-        ZarmView = newTextView()
-        memoryLayout.addView(MemoryView)
-        memoryLayout.addView(ZarmView)
-        memoryLayout.addView(StorageView)
+        TextViewMaps.forEach { (_, view) ->
+            run { memoryLayout.addView(view) }
+        }
         mTxtMemoryViewGroup.addView(memoryLayout)
-    }
-
-    @SuppressLint("SetTextI18n") private fun refreshDate() {
-        val memoryInfo = MemoryUtils().getMemoryInfo(appContext)
-        val storageInfo = MemoryUtils().getStorageInfo(Environment.getExternalStorageDirectory())
-        val swapInfo: MemoryUtils = MemoryUtils().getPartitionInfo("SwapTotal", "SwapFree")
-        MemoryView.text = "运存可用：\t${memoryInfo.availMem.formatSize()} \t| \t总共：\t${memoryInfo.totalMem.formatSize()}"
-        ZarmView.text = "虚拟可用：\t${swapInfo.availMem.formatSize()} \t|\t总共：${swapInfo.totalMem.formatSize()}"
-        StorageView.text = "存储可用：\t${storageInfo.availMem.formatSize()} \t|\t总共：\t${storageInfo.totalMem.formatSize()}"
     }
 
     private fun newTextView(): TextView = TextView(appContext).apply {
         setTextColor(mTxtMemoryInfo1.textColors)
         textSize = 12f
     }
+
+    private fun hideView(it: XC_MethodHook.MethodHookParam) {
+        it.thisObject.putObject("mSeparatorForMemoryInfo", View(appContext))
+        for (i in 0 until mTxtMemoryViewGroup.childCount) {
+            mTxtMemoryViewGroup.getChildAt(i).visibility = View.GONE
+        }
+    }
+
 }
